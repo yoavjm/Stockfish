d74e61aa5c1238cf2561804a10148ac45382d585
Daniel Dugovic <dandydand@gmail.com>
2018-01-10 13:59:55 -0600

    Increase remaining-piece-in-hand weights with previous commit

--- src/position.cpp
+++ src/position.cpp
@@ -1930,10 +1930,10 @@ bool Position::see_ge(Move m, Value threshold) const {
       {
           // Reduce threshold based on remaining material in hand
           if (gives_check(m))
-              threshold -= (type_of(m) == DROP ? material_in_hand(sideToMove, m) : material_in_hand(sideToMove)) / 5;
+              threshold -= (type_of(m) == DROP ? material_in_hand(sideToMove, m) : material_in_hand(sideToMove)) / 2;
           // Increase threshold based on remaining material in hand
           if (checkers())
-              threshold += material_in_hand(~sideToMove, m) / 5;
+              threshold += material_in_hand(~sideToMove, m) / 2;
       }
       // Crazyhouse captures double in value (threshold is halved)
       threshold /= 2;
